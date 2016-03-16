a4158a799194c4e37106b1d1ee286bd6b184871a
vgoni <victor.goni@orbital-aerospace.com>
2016-03-16 14:47:56 +0100

    Improve initial script with messages and optimize

--- bash/workspace/init1.sh
+++ bash/workspace/init1.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 sleep 1
 
+# nice -10, launch process with priority: 10.
+# nice --10, high priority: -10
+# Priority is greater with lesser value.
+# Negative nice value will increase the priority a the process.
+
 # First: open apps
-notify-send "Opening initial apps..."
-guake &
-~/Programs/Telegram/Telegram -- %u &
-thunderbird &
-firefox &
-skype &
-nemo &
+zenity --notification --text="Opening initial apps..."
+nice -5 guake &
+nice -10 ~/Programs/Telegram/Telegram -- %u &
+nice -20 thunderbird &
+nice -10 firefox &
+nice -10 skype &
+nice -10 nemo &
+nice -9 atom &
 # Wait enought time to load all apps
-sleep 30
+zenity --notification --text="Waiting until open..."
+sleep 40
 
 # Second: Move apps (r) to workspaces (t)
-notify-send "Assigning workspaces"
+zenity --notification --text="Assigning workspaces..."
 wmctrl -r ~/Programs/Telegram/Telegram -t 0
 wmctrl -r skype -t 0
 wmctrl -r thunderbird -t 1
 wmctrl -r firefox -t 2
-wmctrl -r nemo -t 3
+wmctrl -r atom -t 4 # text editor
+wmctrl -r Home -t 4 # nemo folder window
 sleep 1
 
 # Third: Focus on firefox
-notify-send "Workspace ready!"
+zenity --info --text="Start to work!"
 wmctrl -a firefox
