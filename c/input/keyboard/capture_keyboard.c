de553a435b14b9e225b6800697edfef0d43d2d8f
vgoni <victor.goni@orbital-aerospace.com>
2016-03-16 12:26:44 +0100

    Update keyboard with c

--- c/input/keyboard/capture_keyboard.c
+++ c/input/keyboard/capture_keyboard.c
@@ -15,7 +15,7 @@
 
 void handler (int sig)
 {
-  printf ("nexiting...(%d)n", sig);
+  printf ("Exiting... signal received: (%d)\n", sig);
   exit (0);
 }
 
@@ -33,33 +33,36 @@ int main (int argc, char *argv[])
   char *device = NULL;
 
   //Setup check
-  if (argv[1] == NULL){
-      printf("Please specify (on the command line) the path to the dev event interface devicen");
-      exit (0);
+  if (argv[1] == NULL)
+  {
+	printf("Bad usage, Specified the path to the dev event interface\n");
+	printf("Sample /dev/input/event5\n");
+	exit (0);
     }
 
   if ((getuid ()) != 0)
-    printf ("You are not root! This may not work...n");
+    printf ("You are not root!\n");
 
   if (argc > 1)
     device = argv[1];
 
   //Open Device
   if ((fd = open (device, O_RDONLY)) == -1)
-    printf ("%s is not a vaild device.n", device);
+    printf ("Device: %s is not a valid device.\n", device);
 
   //Print Device Name
   ioctl (fd, EVIOCGNAME (sizeof (name)), name);
-  printf ("Reading From : %s (%s)n", device, name);
+  printf ("Reading From : %s (%s), forever...\n", device, name);
 
-  while (1){
+  while (1)
+  {
       if ((rd = read (fd, ev, size * 64)) < size)
-          perror_exit ("read()");      
+          perror_exit ("read()\n");      
 
       value = ev[0].value;
 
       if (value != ' ' && ev[1].value == 1 && ev[1].type == 1){ // Only read the key press event
-	   printf ("Code[%d]n", (ev[1].code));
+	   printf ("  Code[%d] received. \n", (ev[1].code));
       }
   }
 
